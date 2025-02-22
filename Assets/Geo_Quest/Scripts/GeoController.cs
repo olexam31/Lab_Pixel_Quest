aa4f34c7fed8b3938c7ade5dd797859cf40afc88
olexam31 <olexandra.public@gmail.com>
2025-02-22 14:06:54 -0500

    added interactable objects

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GeoController : MonoBehaviour
 {
@@ -9,14 +10,15 @@ public class GeoController : MonoBehaviour
     // int, float
     string hello = "hello";
 
-    int numOne = 5;
-    int numTwo = 6;
-
-    float numThree = 7.5f;
+    private Rigidbody2D rb;
+    public int speed = 5;
+    public string nextLevel = "Scene_2";
+  
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         //NumOne = numOne + numTwo;
         // numOne += numTwo;
         // numOne = 5 + 6
@@ -27,10 +29,25 @@ public class GeoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
+        /*
+       if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1, rb.velocity.y);
+
+
+        }
+
+       if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1, rb.velocity.y);
+        }
+        */
         // Debug.Log(hello);
         // numOne++;
         //Debug.Log(numOne);
-
+        /*
         transform.position += new Vector3(0.005f, 0, 0);
 
         if (Input.GetKeyDown(KeyCode.W))
@@ -50,7 +67,33 @@ public class GeoController : MonoBehaviour
             transform.position += new Vector3(-1, 0, 0);
            
         }
+        */
     }  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    Debug.Log("player Has Died");
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                   // without the break statement, it will execute the code in following cases
+              
+                }
+            case "Lose":
+                { 
+                    break;
+             
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
+    }
 
 
 }
